b22bb59bb2eddc6a1e3bcc7dc997790d7bbb4661
Viet Le <vietlq85@gmail.com>
2017-02-15 06:09:34 +0000

    Refactored, added pre-defined functions for productions (post-processors)

--- csscolor.js
+++ csscolor.js
@@ -2,53 +2,93 @@
 // http://github.com/Hardmath123/nearley
 (function () {
 function id(x) {return x[0]; }
+
+
+function retInt(data, location, reject) {
+    return parseInt(data.join(""));
+}
+
+function retFloat(data, location, reject) {
+    return parseFloat(data.join(""));
+}
+
+function triHex(data, location, reject) {
+    return {r: 17*data[1], g: 17*data[2], b: 17*data[3]};
+}
+
+function triHexPair(data, location, reject) {
+    return {r: data[1], g: data[2], b: data[3]};
+}
+
+function retRGB(data, location, reject) {
+    return {r: data[4], g: data[8], b: data[12]};
+}
+
+function retRGBA(data, location, reject) {
+    return {r: data[4], g: data[8], b: data[12], a: data[16]};
+}
+
+function retHSL(data, location, reject) {
+    return {h: data[4], s: data[8], l: data[12]};
+}
+
+function retHSLA(data, location, reject) {
+    return {h: data[4], s: data[8], l: data[12], a: data[16]};
+}
+
 var grammar = {
     ParserRules: [
     {"name": "main", "symbols": ["_", "csscolor", "_"], "postprocess": function(d) {return d[1]; }},
-    {"name": "csscolor$string$1", "symbols": [{"literal":"r"}, {"literal":"g"}, {"literal":"b"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "csscolor", "symbols": ["csscolor$string$1", "_", {"literal":"("}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":")"}, "_"], "postprocess": function(d) { return {r: d[4], g: d[8], b: d[12]}; }},
-    {"name": "csscolor$string$2", "symbols": [{"literal":"r"}, {"literal":"g"}, {"literal":"b"}, {"literal":"a"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "csscolor", "symbols": ["csscolor$string$2", "_", {"literal":"("}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":","}, "_", "max1float", "_", {"literal":")"}, "_"], "postprocess": function(d) { return {r: d[4], g: d[8], b: d[12], a: d[16]}; }},
-    {"name": "csscolor", "symbols": [{"literal":"#"}, "hex", "hex", "hex"], "postprocess": function (d) { return {r: 17*d[1], g: 17*d[2], b: 17*d[3]}; }},
-    {"name": "csscolor", "symbols": [{"literal":"#"}, "hexpair", "hexpair", "hexpair"], "postprocess": function (d) { return {r: d[1], g: d[2], b: d[3]}; }},
-    {"name": "csscolor$string$3", "symbols": [{"literal":"h"}, {"literal":"s"}, {"literal":"l"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "csscolor", "symbols": ["csscolor$string$3", "_", {"literal":"("}, "_", "max360", "_", {"literal":","}, "_", "percent", "_", {"literal":","}, "_", "percent", "_", {"literal":")"}], "postprocess": function(d) { return {h: d[4], s: d[8], l: d[12]}; }},
-    {"name": "csscolor$string$4", "symbols": [{"literal":"h"}, {"literal":"s"}, {"literal":"l"}, {"literal":"a"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "csscolor", "symbols": ["csscolor$string$4", "_", {"literal":"("}, "_", "max360", "_", {"literal":","}, "_", "percent", "_", {"literal":","}, "_", "percent", "_", {"literal":","}, "_", "max1float", "_", {"literal":")"}], "postprocess": function(d) { return {h: d[4], s: d[8], l: d[12], a: d[16]}; }},
+    {"name": "csscolor", "symbols": ["rgb"], "postprocess": id},
+    {"name": "csscolor", "symbols": ["rgba"], "postprocess": id},
+    {"name": "csscolor", "symbols": ["trihex"], "postprocess": id},
+    {"name": "csscolor", "symbols": ["trihexpair"], "postprocess": id},
+    {"name": "csscolor", "symbols": ["hsl"], "postprocess": id},
+    {"name": "csscolor", "symbols": ["hsla"], "postprocess": id},
+    {"name": "rgb$string$1", "symbols": [{"literal":"r"}, {"literal":"g"}, {"literal":"b"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "rgb", "symbols": ["rgb$string$1", "_", {"literal":"("}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":")"}, "_"], "postprocess": retRGB},
+    {"name": "rgba$string$1", "symbols": [{"literal":"r"}, {"literal":"g"}, {"literal":"b"}, {"literal":"a"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "rgba", "symbols": ["rgba$string$1", "_", {"literal":"("}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":","}, "_", "max255", "_", {"literal":","}, "_", "max1float", "_", {"literal":")"}, "_"], "postprocess": retRGBA},
+    {"name": "trihex", "symbols": [{"literal":"#"}, "hex", "hex", "hex"], "postprocess": triHex},
+    {"name": "trihexpair", "symbols": [{"literal":"#"}, "hexpair", "hexpair", "hexpair"], "postprocess": triHexPair},
+    {"name": "hsl$string$1", "symbols": [{"literal":"h"}, {"literal":"s"}, {"literal":"l"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "hsl", "symbols": ["hsl$string$1", "_", {"literal":"("}, "_", "max360", "_", {"literal":","}, "_", "percent", "_", {"literal":","}, "_", "percent", "_", {"literal":")"}], "postprocess": retHSL},
+    {"name": "hsla$string$1", "symbols": [{"literal":"h"}, {"literal":"s"}, {"literal":"l"}, {"literal":"a"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "hsla", "symbols": ["hsla$string$1", "_", {"literal":"("}, "_", "max360", "_", {"literal":","}, "_", "percent", "_", {"literal":","}, "_", "percent", "_", {"literal":","}, "_", "max1float", "_", {"literal":")"}], "postprocess": retHSLA},
     {"name": "max255$ebnf$1", "symbols": [/[1-9]/], "postprocess": id},
     {"name": "max255$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "max255", "symbols": ["max255$ebnf$1", /[0-9]/], "postprocess": function(d) { return parseInt(d.join("")); }},
-    {"name": "max255", "symbols": [{"literal":"1"}, /[0-9]/, /[0-9]/], "postprocess": function(d) { return parseInt(d.join("")); }},
-    {"name": "max255", "symbols": [{"literal":"2"}, /[0-5]/, /[0-5]/], "postprocess": function(d) { return parseInt(d.join("")); }},
+    {"name": "max255", "symbols": ["max255$ebnf$1", /[0-9]/], "postprocess": retInt},
+    {"name": "max255", "symbols": [{"literal":"1"}, /[0-9]/, /[0-9]/], "postprocess": retInt},
+    {"name": "max255", "symbols": [{"literal":"2"}, /[0-5]/, /[0-5]/], "postprocess": retInt},
     {"name": "max360$ebnf$1", "symbols": [/[1-9]/], "postprocess": id},
     {"name": "max360$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "max360", "symbols": ["max360$ebnf$1", /[0-9]/], "postprocess": function(d) { return parseInt(d.join("")); }},
-    {"name": "max360", "symbols": [/[1-2]/, /[0-9]/, /[0-9]/], "postprocess": function(d) { return parseInt(d.join("")); }},
-    {"name": "max360", "symbols": [{"literal":"3"}, /[0-5]/, /[0-9]/], "postprocess": function(d) { return parseInt(d.join("")); }},
+    {"name": "max360", "symbols": ["max360$ebnf$1", /[0-9]/], "postprocess": retInt},
+    {"name": "max360", "symbols": [/[1-2]/, /[0-9]/, /[0-9]/], "postprocess": retInt},
+    {"name": "max360", "symbols": [{"literal":"3"}, /[0-5]/, /[0-9]/], "postprocess": retInt},
     {"name": "max360$string$1", "symbols": [{"literal":"3"}, {"literal":"6"}, {"literal":"0"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "max360", "symbols": ["max360$string$1"], "postprocess": function(d) { return parseInt(d.join("")); }},
+    {"name": "max360", "symbols": ["max360$string$1"], "postprocess": retInt},
     {"name": "percent", "symbols": ["max100float", {"literal":"%"}], "postprocess": function(d) { return (0.01*d[0]); }},
     {"name": "max100float$ebnf$1", "symbols": [/[0-9]/]},
     {"name": "max100float$ebnf$1", "symbols": [/[0-9]/, "max100float$ebnf$1"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
-    {"name": "max100float", "symbols": [{"literal":"."}, "max100float$ebnf$1"], "postprocess": function(d) { return parseFloat(d.join("")); }},
+    {"name": "max100float", "symbols": [{"literal":"."}, "max100float$ebnf$1"], "postprocess": retFloat},
     {"name": "max100float$ebnf$2", "symbols": [/[1-9]/], "postprocess": id},
     {"name": "max100float$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
     {"name": "max100float$ebnf$3", "symbols": [{"literal":"."}], "postprocess": id},
     {"name": "max100float$ebnf$3", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "max100float", "symbols": ["max100float$ebnf$2", /[0-9]/, "max100float$ebnf$3"], "postprocess": function(d) { return parseFloat(d.join("")); }},
+    {"name": "max100float", "symbols": ["max100float$ebnf$2", /[0-9]/, "max100float$ebnf$3"], "postprocess": retFloat},
     {"name": "max100float$ebnf$4", "symbols": [/[1-9]/], "postprocess": id},
     {"name": "max100float$ebnf$4", "symbols": [], "postprocess": function(d) {return null;}},
     {"name": "max100float$ebnf$5", "symbols": [/[0-9]/]},
     {"name": "max100float$ebnf$5", "symbols": [/[0-9]/, "max100float$ebnf$5"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
-    {"name": "max100float", "symbols": ["max100float$ebnf$4", /[0-9]/, {"literal":"."}, "max100float$ebnf$5"], "postprocess": function(d) { return parseFloat(d.join("")); }},
+    {"name": "max100float", "symbols": ["max100float$ebnf$4", /[0-9]/, {"literal":"."}, "max100float$ebnf$5"], "postprocess": retFloat},
     {"name": "max100float$string$1", "symbols": [{"literal":"1"}, {"literal":"0"}, {"literal":"0"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "max100float$ebnf$6", "symbols": [{"literal":"."}], "postprocess": id},
     {"name": "max100float$ebnf$6", "symbols": [], "postprocess": function(d) {return null;}},
-    {"name": "max100float", "symbols": ["max100float$string$1", "max100float$ebnf$6"], "postprocess": function(d) { return parseFloat(d.join("")); }},
+    {"name": "max100float", "symbols": ["max100float$string$1", "max100float$ebnf$6"], "postprocess": retFloat},
     {"name": "max100float$string$2", "symbols": [{"literal":"1"}, {"literal":"0"}, {"literal":"0"}, {"literal":"."}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "max100float$ebnf$7", "symbols": [{"literal":"0"}]},
     {"name": "max100float$ebnf$7", "symbols": [{"literal":"0"}, "max100float$ebnf$7"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
-    {"name": "max100float", "symbols": ["max100float$string$2", "max100float$ebnf$7"], "postprocess": function(d) { return parseFloat(d.join("")); }},
+    {"name": "max100float", "symbols": ["max100float$string$2", "max100float$ebnf$7"], "postprocess": retFloat},
     {"name": "max1float$ebnf$1", "symbols": [{"literal":"."}], "postprocess": id},
     {"name": "max1float$ebnf$1", "symbols": [], "postprocess": function(d) {return null;}},
     {"name": "max1float", "symbols": [{"literal":"0"}, "max1float$ebnf$1"], "postprocess": function(d) { return 0.0; }},
@@ -56,7 +96,7 @@ var grammar = {
     {"name": "max1float$ebnf$2", "symbols": [], "postprocess": function(d) {return null;}},
     {"name": "max1float$ebnf$3", "symbols": [/[0-9]/]},
     {"name": "max1float$ebnf$3", "symbols": [/[0-9]/, "max1float$ebnf$3"], "postprocess": function arrconcat(d) {return [d[0]].concat(d[1]);}},
-    {"name": "max1float", "symbols": ["max1float$ebnf$2", {"literal":"."}, "max1float$ebnf$3"], "postprocess": function(d) { return parseFloat(d.join("")); }},
+    {"name": "max1float", "symbols": ["max1float$ebnf$2", {"literal":"."}, "max1float$ebnf$3"], "postprocess": retFloat},
     {"name": "max1float$ebnf$4", "symbols": [{"literal":"."}], "postprocess": id},
     {"name": "max1float$ebnf$4", "symbols": [], "postprocess": function(d) {return null;}},
     {"name": "max1float", "symbols": [{"literal":"1"}, "max1float$ebnf$4"], "postprocess": function(d) { return 1.0; }},
--- csscolor.ne
+++ csscolor.ne
@@ -1,33 +1,81 @@
 # https://www.w3schools.com/cssref/css_colors_legal.asp
 
+@{%
+
+function retInt(data, location, reject) {
+    return parseInt(data.join(""));
+}
+
+function retFloat(data, location, reject) {
+    return parseFloat(data.join(""));
+}
+
+function triHex(data, location, reject) {
+    return {r: 17*data[1], g: 17*data[2], b: 17*data[3]};
+}
+
+function triHexPair(data, location, reject) {
+    return {r: data[1], g: data[2], b: data[3]};
+}
+
+function retRGB(data, location, reject) {
+    return {r: data[4], g: data[8], b: data[12]};
+}
+
+function retRGBA(data, location, reject) {
+    return {r: data[4], g: data[8], b: data[12], a: data[16]};
+}
+
+function retHSL(data, location, reject) {
+    return {h: data[4], s: data[8], l: data[12]};
+}
+
+function retHSLA(data, location, reject) {
+    return {h: data[4], s: data[8], l: data[12], a: data[16]};
+}
+
+%}
+
 main -> _ csscolor _ {% function(d) {return d[1]; } %}
 
-csscolor -> "rgb" _ "(" _ max255 _ "," _ max255 _ "," _ max255  _ ")" _ {% function(d) { return {r: d[4], g: d[8], b: d[12]}; } %}
-    | "rgba" _ "(" _ max255 _ "," _ max255 _ "," _ max255  _ "," _ max1float  _ ")" _ {% function(d) { return {r: d[4], g: d[8], b: d[12], a: d[16]}; } %}
-    | "#" hex hex hex {% function (d) { return {r: 17*d[1], g: 17*d[2], b: 17*d[3]}; } %}
-    | "#" hexpair hexpair hexpair {% function (d) { return {r: d[1], g: d[2], b: d[3]}; } %}
-    | "hsl" _ "(" _ max360 _ "," _ percent _ "," _ percent  _ ")" {% function(d) { return {h: d[4], s: d[8], l: d[12]}; } %}
-    | "hsla" _ "(" _ max360 _ "," _ percent _ "," _ percent _ "," _ max1float _ ")" {% function(d) { return {h: d[4], s: d[8], l: d[12], a: d[16]}; } %}
+csscolor -> rgb     {% id %}
+    | rgba          {% id %}
+    | trihex        {% id %}
+    | trihexpair    {% id %}
+    | hsl           {% id %}
+    | hsla          {% id %}
 
-max255 -> [1-9]:? [0-9] {% function(d) { return parseInt(d.join("")); } %}
-        | "1" [0-9] [0-9] {% function(d) { return parseInt(d.join("")); } %}
-        | "2" [0-5] [0-5] {% function(d) { return parseInt(d.join("")); } %}
+rgb -> "rgb" _ "(" _ max255 _ "," _ max255 _ "," _ max255  _ ")" _ {% retRGB %}
 
-max360 -> [1-9]:? [0-9] {% function(d) { return parseInt(d.join("")); } %}
-        | [1-2] [0-9] [0-9] {% function(d) { return parseInt(d.join("")); } %}
-        | "3" [0-5] [0-9] {% function(d) { return parseInt(d.join("")); } %}
-        | "360" {% function(d) { return parseInt(d.join("")); } %}
+rgba -> "rgba" _ "(" _ max255 _ "," _ max255 _ "," _ max255  _ "," _ max1float  _ ")" _ {% retRGBA %}
+
+trihex -> "#" hex hex hex {% triHex %}
+
+trihexpair -> "#" hexpair hexpair hexpair {% triHexPair %}
+
+hsl -> "hsl" _ "(" _ max360 _ "," _ percent _ "," _ percent  _ ")" {% retHSL %}
+
+hsla -> "hsla" _ "(" _ max360 _ "," _ percent _ "," _ percent _ "," _ max1float _ ")" {% retHSLA %}
+
+max255 -> [1-9]:? [0-9] {% retInt %}
+        | "1" [0-9] [0-9] {% retInt %}
+        | "2" [0-5] [0-5] {% retInt %}
+
+max360 -> [1-9]:? [0-9] {% retInt %}
+        | [1-2] [0-9] [0-9] {% retInt %}
+        | "3" [0-5] [0-9] {% retInt %}
+        | "360" {% retInt %}
 
 percent -> max100float "%" {% function(d) { return (0.01*d[0]); } %}
 
-max100float -> "." [0-9]:+ {% function(d) { return parseFloat(d.join("")); } %}
-        | [1-9]:? [0-9] ".":? {% function(d) { return parseFloat(d.join("")); } %}
-        | [1-9]:? [0-9] "." [0-9]:+ {% function(d) { return parseFloat(d.join("")); } %}
-        | "100" ".":? {% function(d) { return parseFloat(d.join("")); } %}
-        | "100." "0":+ {% function(d) { return parseFloat(d.join("")); } %}
+max100float -> "." [0-9]:+ {% retFloat %}
+        | [1-9]:? [0-9] ".":? {% retFloat %}
+        | [1-9]:? [0-9] "." [0-9]:+ {% retFloat %}
+        | "100" ".":? {% retFloat %}
+        | "100." "0":+ {% retFloat %}
 
 max1float -> "0" ".":? {% function(d) { return 0.0; } %}
-        | "0":? "." [0-9]:+ {% function(d) { return parseFloat(d.join("")); } %}
+        | "0":? "." [0-9]:+ {% retFloat %}
         | "1" ".":? {% function(d) { return 1.0; } %}
         | "1" "." "0":* {% function(d) { return 1.0; } %}
 
