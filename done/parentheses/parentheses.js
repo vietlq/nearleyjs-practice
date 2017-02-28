f4b80efa23a16e744dd955c46ae341b1c0a84b98
Viet Le <vietlq85@gmail.com>
2017-02-28 21:55:47 +0000

    Fixed parentheses

---
+++ done/parentheses/parentheses.js
@@ -0,0 +1,24 @@
+// Generated automatically by nearley
+// http://github.com/Hardmath123/nearley
+(function () {
+function id(x) {return x[0]; }
+ function TRUE (d) { return true; } var grammar = {
+    ParserRules: [
+    {"name": "P", "symbols": [{"literal":"("}, "E", {"literal":")"}], "postprocess": TRUE},
+    {"name": "P", "symbols": [{"literal":"{"}, "E", {"literal":"}"}], "postprocess": TRUE},
+    {"name": "P", "symbols": [{"literal":"["}, "E", {"literal":"]"}], "postprocess": TRUE},
+    {"name": "P", "symbols": [{"literal":"<"}, "E", {"literal":">"}], "postprocess": TRUE},
+    {"name": "E", "symbols": []},
+    {"name": "E", "symbols": [{"literal":"("}, "E", {"literal":")"}, "E"]},
+    {"name": "E", "symbols": [{"literal":"{"}, "E", {"literal":"}"}, "E"]},
+    {"name": "E", "symbols": [{"literal":"["}, "E", {"literal":"]"}, "E"]},
+    {"name": "E", "symbols": [{"literal":"<"}, "E", {"literal":">"}, "E"]}
+]
+  , ParserStart: "P"
+}
+if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
+   module.exports = grammar;
+} else {
+   window.grammar = grammar;
+}
+})();
